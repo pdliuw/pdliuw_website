5d85e4762f18b60ea7da6c6df9e7dce3210bc0f4
刘攀登 <non@non.com>
2021-01-27 18:21:57 +0800

    update sort model

---
+++ lib/config/sort_config.dart
@@ -0,0 +1,42 @@
+class SortConfig {
+  ///
+  /// sort
+  static final List<dynamic> sortList = [
+    {
+      "name": "日常随记",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "软件设计",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Android",
+      "backgroundAssetPath": "assets/design_pattern_one_logo.jpg",
+    },
+    {
+      "name": "Java",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Flutter",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "版本管理",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "文学",
+      "backgroundAssetPath": "assets/dart_logo.jpg",
+    },
+    {
+      "name": "项目示例",
+      "backgroundAssetPath": "assets/dart_logo.jpg",
+    },
+    {
+      "name": "其他",
+      "backgroundAssetPath": "assets/dart_logo.jpg",
+    },
+  ];
+}
--- lib/sort/sort_home_page.dart
+++ lib/sort/sort_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_radar_chart/flutter_radar_chart.dart';
+import 'package:pdliuw_website/config/sort_config.dart';
 import 'package:pdliuw_website/widget/air_app_bar_factory.dart';
 
 ///
@@ -9,10 +11,117 @@ class SortHomePage extends StatefulWidget {
 }
 
 class _SortHomePageState extends State<SortHomePage> {
+  bool darkMode = false;
+  bool useSides = false;
+  double numberOfFeatures = 3;
+
+  List<int> ticks;
+  List<String> features;
+  List<List<int>> data;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    ticks = [1, 5, 7, 14, 21, 28, 35];
+    features = SortConfig.sortList.map((e) => "${e['name']}").toList();
+    data = [
+      [10, 20, 28, 5, 16, 15, 23, 19, 10],
+      [15, 1, 4, 14, 23, 10, 22, 35, 10]
+    ];
+    //filter data
+    features = features.sublist(0, numberOfFeatures.floor());
+    data = data
+        .map((graph) => graph.sublist(0, numberOfFeatures.floor()))
+        .toList();
+
     return Scaffold(
       appBar: AirAppBarFactory.appBar(titleName: "分类"),
+      body: Container(
+        margin: EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
+        color: darkMode ? Colors.black : Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  useSides
+                      ? Text(
+                          'Polygon border',
+                          style: darkMode
+                              ? TextStyle(color: Colors.white)
+                              : TextStyle(color: Colors.black),
+                        )
+                      : Text(
+                          'Circular border',
+                          style: darkMode
+                              ? TextStyle(color: Colors.white)
+                              : TextStyle(color: Colors.black),
+                        ),
+                  Switch(
+                    value: this.useSides,
+                    onChanged: (value) {
+                      setState(() {
+                        useSides = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    'Number of features',
+                    style: TextStyle(
+                        color: darkMode ? Colors.white : Colors.black),
+                  ),
+                  Expanded(
+                    child: Slider(
+                      value: this.numberOfFeatures,
+                      min: 3,
+                      max: 8,
+                      divisions: 5,
+                      onChanged: (value) {
+                        setState(() {
+                          numberOfFeatures = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: darkMode
+                  ? RadarChart.dark(
+                      ticks: ticks,
+                      features: features,
+                      data: data,
+                      reverseAxis: true,
+                      useSides: useSides,
+                    )
+                  : RadarChart.light(
+                      ticks: ticks,
+                      features: features,
+                      data: data,
+                      reverseAxis: true,
+                      useSides: useSides,
+                    ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
