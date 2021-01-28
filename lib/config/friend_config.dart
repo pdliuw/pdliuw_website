2a410a3805968704490645fbc30385965babcff4
刘攀登 <non@non.com>
2021-01-28 15:07:37 +0800

    optimize person model

---
+++ lib/config/friend_config.dart
@@ -0,0 +1,41 @@
+class FriendConfig {
+  ///
+  /// friend
+  static final List<dynamic> friendList = [
+    {
+      "avatar": "http://image.luokangyuan.com/1_qq_27922023.jpg",
+      "name": "码酱",
+      "introduction": "......",
+      "url": "http://luokangyuan.com/",
+      "title": "前去学习"
+    },
+    {
+      "avatar": "https://pdliuw.github.io/medias/avatar.jpg",
+      "name": "Air",
+      "introduction": "......",
+      "url": "https://pdliuw.github.io/",
+      "title": "前去学习"
+    },
+    {
+      "avatar": "https://jiangliuhong.gitee.io/images/avatar.jpg",
+      "name": "编程常青树",
+      "introduction": "......",
+      "url": "https://jiangliuhong.gitee.io/",
+      "title": "前去学习"
+    },
+    {
+      "avatar": "https://sunhwee.com/favicon.png",
+      "name": "洪卫の博客",
+      "introduction": "......",
+      "url": "https://sunhwee.com",
+      "title": "前去探索"
+    },
+    {
+      "avatar": "http://jiguankai.cn/img/favicon.ico",
+      "name": "OldJii",
+      "introduction": "老“纪”伏枥，志在千里",
+      "url": "http://jiguankai.cn/",
+      "title": "前去探索"
+    }
+  ];
+}
--- lib/friend/friend_home_page.dart
+++ lib/friend/friend_home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pdliuw_website/config/friend_config.dart';
+import 'package:pdliuw_website/helper/launcher_helper.dart';
 import 'package:pdliuw_website/widget/air_app_bar_factory.dart';
+import 'package:pdliuw_website/widget/air_container_list_item_widget.dart';
+import 'package:pdliuw_website/widget/air_text_widget.dart';
 
 ///
 /// FriendHomePage
@@ -13,6 +17,42 @@ class _FriendHomePageState extends State<FriendHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AirAppBarFactory.appBar(titleName: "友情链接"),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: FriendConfig.friendList
+            .map((e) => AirContainerListItemWidget.defaultStyle(
+                    child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(e['avatar']),
+                      onBackgroundImageError: (object, stackTrace) {},
+                      radius: 30,
+                    ),
+                    AirTextWidget.defaultStyle(
+                      data: e['name'],
+                      textSize: AirTextSize.middle4,
+                    ),
+                    AirTextWidget.defaultStyle(
+                      data: e['introduction'],
+                    ),
+                    Spacer(),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        _openFriendWebsite(e['url']);
+                      },
+                      icon: Icon(Icons.looks),
+                      label: Text("${e['title']}"),
+                    ),
+                  ],
+                )))
+            .toList(),
+      ),
     );
+  }
+
+  ///
+  /// open url
+  _openFriendWebsite(String url) {
+    LauncherHelper.openUrl(url: url);
   }
 }
