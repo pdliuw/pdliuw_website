0c80502916f081572102c65223dd39566d73b0cb
刘攀登 <non@non.com>
2021-01-28 11:00:46 +0800

    optimize archive model

--- lib/archive/archive_home_page.dart
+++ lib/archive/archive_home_page.dart
@@ -1,5 +1,8 @@
+import 'package:bubble_timeline/timeline_item.dart';
 import 'package:flutter/material.dart';
+import 'package:pdliuw_website/config/archive_config.dart';
 import 'package:pdliuw_website/widget/air_app_bar_factory.dart';
+import 'package:bubble_timeline/bubble_timeline.dart';
 
 ///
 /// ArchiveHomePage
@@ -9,10 +12,33 @@ class ArchiveHomePage extends StatefulWidget {
 }
 
 class _ArchiveHomePageState extends State<ArchiveHomePage> {
+  double _bubbleRadius = 120;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AirAppBarFactory.appBar(titleName: "归档"),
+      body: BubbleTimeline(
+        bubbleDiameter: _bubbleRadius,
+        items: ArchiveConfig.archiveList
+            .map((e) => TimelineItem(
+                  title: e['name'],
+                  description: e["description"],
+                  child: Container(
+                    width: _bubbleRadius,
+                    height: _bubbleRadius,
+                    child: ClipOval(
+                      child: Image.asset(
+                        e['backgroundAssetPath'],
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  bubbleColor: Colors.green,
+                ))
+            .toList(),
+        stripColor: Colors.teal,
+        scaffoldColor: Colors.white,
+      ),
     );
   }
 }
---
+++ lib/config/archive_config.dart
@@ -0,0 +1,116 @@
+class ArchiveConfig {
+  ///
+  /// archive
+  static final List<dynamic> archiveList = [
+    {
+      "name": "日常随记",
+      "description": "日常随记",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Java",
+      "description": "development language",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "DesignPatterns",
+      "description": "DesignPatterns",
+      "backgroundAssetPath": "assets/design_pattern_one_logo.jpg",
+    },
+    {
+      "name": "Windows",
+      "description": "Windows",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "View",
+      "description": "View",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Permission",
+      "description": "Permission",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Dart",
+      "description": "development language",
+      "backgroundAssetPath": "assets/dart_logo.jpg",
+    },
+    {
+      "name": "Flutter",
+      "description": "develop mobile web ui kit",
+      "backgroundAssetPath": "assets/flutter_logo.jpg",
+    },
+    {
+      "name": "Route",
+      "description": "flutter route",
+      "backgroundAssetPath": "assets/flutter_logo.jpg",
+    },
+    {
+      "name": "Git",
+      "description": "project/version manager tool,",
+      "backgroundAssetPath": "assets/git_logo.jpg",
+    },
+    {
+      "name": "Github",
+      "description": "open source project manager tool powered by git",
+      "backgroundAssetPath": "assets/github_logo.jpg",
+    },
+    {
+      "name": "barcode",
+      "description": "barcode",
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "Kotlin",
+      "description": "development language",
+      "backgroundAssetPath": "assets/kotlin_logo.jpg",
+    },
+    {
+      "name": "Swift",
+      "description": "development language",
+      "backgroundAssetPath": "assets/swift_logo.jpg",
+    },
+    {
+      "name": "JavaScript",
+      "description": "development language",
+      "backgroundAssetPath": "assets/js_logo.jpg",
+    },
+    {
+      "name": "RxJava",
+      "description": "android thread manage library",
+      "backgroundAssetPath": "assets/android_one_logo.jpg",
+    },
+    {
+      "name": "Retrofit",
+      "description": "android http annotation library",
+      "backgroundAssetPath": "assets/android_one_logo.jpg",
+    },
+    {
+      "name": "OKHttp",
+      "description": "android http library",
+      "backgroundAssetPath": "assets/android_one_logo.jpg",
+    },
+    {
+      "name": "Glide",
+      "description": "android image library",
+      "backgroundAssetPath": "assets/android_one_logo.jpg",
+    },
+    {
+      "name": "MVP",
+      "description": "one of the architecture",
+      "backgroundAssetPath": "assets/design_pattern_logo.jpg",
+    },
+    {
+      "name": "Dio",
+      "description": "flutter http package",
+      "backgroundAssetPath": "assets/flutter_logo.jpg",
+    },
+    {
+      "name": "贝塞尔曲线",
+      "description": "ui design think",
+      "backgroundAssetPath": "assets/android_one_logo.jpg",
+    },
+  ];
+}
