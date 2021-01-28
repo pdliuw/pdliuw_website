1840cd786222172c4d3da76bfdd130ae3c004bae
刘攀登 <non@non.com>
2021-01-28 11:32:18 +0800

    optimize person model

---
+++ lib/config/person_config.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class PersonConfig {
+  ///
+  /// person
+  static final List<dynamic> personChartDataList = [
+    {
+      "name": "日常随记",
+      "value": 5.0,
+      "color": Colors.green,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 6.0,
+      "color": Colors.grey,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 7.0,
+      "color": Colors.orange,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 8.0,
+      "color": Colors.blue,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 9.0,
+      "color": Colors.yellow,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 10.0,
+      "color": Colors.teal,
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 11.0,
+      "color": Colors.green[200],
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 12.0,
+      "color": Colors.grey[200],
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 13.0,
+      "color": Colors.orange[200],
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 14.0,
+      "color": Colors.blue[200],
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+    {
+      "name": "日常随记",
+      "value": 15.0,
+      "color": Colors.yellow[200],
+      "backgroundAssetPath": "assets/daily_note_logo.jpg",
+    },
+  ];
+}
--- lib/person/person_home_page.dart
+++ lib/person/person_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pdliuw_website/config/person_config.dart';
 import 'package:pdliuw_website/widget/air_app_bar_factory.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 ///
 /// PersonHomePage
@@ -9,10 +11,49 @@ class PersonHomePage extends StatefulWidget {
 }
 
 class _PersonHomePageState extends State<PersonHomePage> {
+  List<PieChartSectionData> _pieSectionDataList = [];
+  PieTouchData _pieTouchData = PieTouchData(touchCallback: (response) {});
+  @override
+  void initState() {
+    super.initState();
+
+    PersonConfig.personChartDataList.forEach((element) {
+      String name = element['name'];
+      double value = element['value'];
+      Color color = element['color'];
+
+      _pieSectionDataList.add(
+        PieChartSectionData(
+          value: value,
+          color: color,
+          title: "$value",
+          showTitle: true,
+          titlePositionPercentageOffset: 0.5, //0.5:center
+        ),
+      );
+
+      _pieTouchData.props.add(value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AirAppBarFactory.appBar(titleName: "关于"),
-    );
+        appBar: AirAppBarFactory.appBar(titleName: "关于"),
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: PieChart(
+                  PieChartData(
+                    sections: _pieSectionDataList,
+                    pieTouchData: _pieTouchData,
+                  ),
+                  swapAnimationDuration: Duration(seconds: 2),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
